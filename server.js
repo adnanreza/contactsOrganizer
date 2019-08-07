9fcc02689b3dc3e778c7393e0975b716592c9369
Adnan Reza <adnan.reza@outlook.com>
2019-08-07 12:31:30 -0700

    Authenticate route

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) =>
 
 // Define backend routes
 app.use("/api/users", require("./routes/users"));
-app.use("/api/auth", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
