890812c8242f5a84febfe3cdc7158f95262204ba
Adnan Reza <adnan.reza@outlook.com>
2019-08-11 12:59:07 -0700

    Created AlertState and UI Component

---
+++ client/src/components/layout/Alerts.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
+
+const Alerts = () => {
+  const alertContext = useContext(AlertContext);
+
+  return (
+    // if there is anything in alerts array, loop through and output
+    alertContext.alerts.length > 0 &&
+    alertContext.alerts.map(alert => (
+      <div key={alert.id} className={`alert alert-${alert.type}`}>
+        <i className='fas fa-info-circle' /> {alert.msg}
+      </div>
+    ))
+  );
+};
+
+export default Alerts;
