3fd13726d3a172a168bb4609c394c691fbd183b7
Adnan Reza <adnan.reza@outlook.com>
2019-08-12 11:27:37 -0700

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express"); //Common JS syntax for importing express
 const connectDB = require("./config/db");
+const path = require('path');
 
 const app = express();
 
@@ -10,13 +11,21 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
-  res.json({ msg: "Welcome to the ContactsOrganizer API" })
+  //res.json({ msg: "Welcome to the ContactsOrganizer API" })
 );
 
 // Define backend routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirnane, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
