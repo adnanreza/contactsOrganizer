9e3aaab78bde428f8af7844a41e27153f846eb1b
Adnan Reza <adnan.reza@outlook.com>
2020-06-30 21:42:13 -0700

    Passed option useUnifiedTopology to MongoClient

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const db = config.get("mongoURI");
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
-      useFindAndModify: false
+      useFindAndModify: false,
+      useUnifiedTopology: true,
     });
-    console.log("MongoDB connected...");
+    console.log('MongoDB connected...');
   } catch (err) {
     console.log(err.message);
     process.exit(1);
