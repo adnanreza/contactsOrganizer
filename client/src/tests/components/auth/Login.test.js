d699a295cabdbc5138e32ba72c63a9631513477b
Adnan Reza <adnan.reza@outlook.com>
2020-06-30 21:22:52 -0700

    Created tests directory

---
+++ client/src/tests/components/auth/Login.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Login } from '../../../components/auth/Login';
+
+let login, clearErrors, isAuthenticated, error, setAlert, wrapper;
+
+// beforeEach(() => {
+//   wrapper = shallow(<Login />);
+// });
+
+// test('should render Login component correctly', () => {
+//   expect(wrapper).toMatchSnapshot();
+// });
