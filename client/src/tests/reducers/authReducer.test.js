821a8835db30c0a4aac0fa3e1a70fac3aa5c2e45
Adnan Reza <adnan.reza@outlook.com>
2020-06-30 22:42:29 -0700

    Added tests to test reducer cases for LOGOUT and CLEAR_ERRORS

--- client/src/tests/reducers/authReducer.test.js
+++ client/src/tests/reducers/authReducer.test.js
@@ -19,3 +19,30 @@ test('should load user correctly', () => {
   expect(state.loading).toEqual(false);
   expect(state.isAuthenticated).toEqual(true);
 });
+
+// test('should register user correctly', () => {})
+
+// test('should login user correctly', () => {
+
+// })
+
+test('should logout user correctly', () => {
+  const action = {
+    type: 'LOGOUT',
+  };
+  const state = authReducer(initialState, action);
+  expect(state.token).toEqual(null);
+  expect(state.loading).toEqual(false);
+  expect(state.isAuthenticated).toEqual(false);
+  expect(state.user).toEqual(null);
+});
+
+test('should clear errors correctly', () => {
+  initialState.error = 'errors';
+  console.log(initialState);
+  const action = {
+    type: 'CLEAR_ERRORS',
+  };
+  const state = authReducer(initialState, action);
+  expect(state.error).toEqual(null);
+});
