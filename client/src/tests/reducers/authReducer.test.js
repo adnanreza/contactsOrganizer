b1ce1896e8bbffed09d98be31d7d25b1874401fe
Adnan Reza <adnan.reza@outlook.com>
2020-06-30 22:30:05 -0700

    Wrote test to check if reducer handled USER_LOADED correctly

---
+++ client/src/tests/reducers/authReducer.test.js
@@ -0,0 +1,22 @@
+import authReducer from '../../context/auth/authReducer';
+import axios from 'axios';
+
+const initialState = {
+  token: localStorage.getItem('token'),
+  isAuthenticated: null,
+  loading: true,
+  user: null,
+  error: null,
+};
+
+test('should load user correctly', () => {
+  const action = {
+    type: 'USER_LOADED',
+    payload: 'newUser',
+  };
+
+  const state = authReducer(initialState, action);
+  expect(state.user).toEqual('newUser');
+  expect(state.loading).toEqual(false);
+  expect(state.isAuthenticated).toEqual(true);
+});
